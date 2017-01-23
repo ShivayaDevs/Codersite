2859e6aac3cdd0bb1fa988e222367d3d186f0000
vagisha-nidhi <vagisha.nda@gmail.com>
2017-01-23 21:01:16 +0530

    Article class

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from Codersite.models import User
 
 # Create your models here.
+
+class Article(models.Model) :
+    article_text = models.CharField(max_length=1000)
+    # user = models.ForeignKey(User)
